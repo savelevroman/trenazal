0eda47223b300997ebfea5023b013a1b47c43fff
savelevroman <160472221+savelevroman@users.noreply.github.com>
2024-09-19 15:00:20 +0700

    основа

---
+++ template/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Тренажерный зал</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css"> 
+<link href= "css/bootstrap.min.css" rel="stylesheet" type="text/css"> 
+<script src="js/bootstrap.js"></script> 
+</head>
+<body>
