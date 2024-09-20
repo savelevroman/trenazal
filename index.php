05a08bb3f5968a4c0b6d2214ea1098450203e4f4
ShevchenkoW3 <shevdan03@yandex.ru>
2024-09-20 12:36:25 +0300

    Выравнивание заголовка

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ include 'template/head.php';
 include 'template/nav.php';
 ?>
 <div class="row">
-    <div class="col-2"></div>
+    <div class="col-1"></div>
     <div class="col-10 text-center">
         <h3>Тренажерный зал</h3>
     </div>
-    <div class="col-2"></div>
+    <div class="col-1"></div>
 </div>
